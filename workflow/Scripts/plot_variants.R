c16a16d104b455f73e2191554271e9b74e9f51d7
Kyreria <dennishaandrikman@live.nl>
2024-06-24 22:33:32 +0200

    Updated the R script to plot a VCF plot using the plot-VCF package.

--- workflow/Scripts/plot_variants.R
+++ workflow/Scripts/plot_variants.R
@@ -1,3 +1,12 @@
+# Get the packages needed for visualisation.
+if (!require("devtools")) install.packages("devtools")
+if (!require("BiocManager")) install.packages("BiocManager")
+remotes::install_github(
+    "cccnrc/plot-VCF",
+    repos = BiocManager::repositories()
+)
+
+library(plotVCF)
 
 # Get command line arguments
 args <- commandArgs(trailingOnly = TRUE)
@@ -9,3 +18,12 @@ data <- read.table(vcf_file, header= FALSE, comment.char= "#", stringsAsFactors 
 
 #Since colnames aren't available, use VCF format.
 colnames(data) <- c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO")
+
+# Making use of the new "package, plotVCF
+
+VCF_Plot <- createVCFplot(vcf_file, ORDERED=TRUE)
+PNG_File <- output
+
+png(PNG_File, width=5000, height=2500, res=300)
+print(VCF_Plot)
+dev.off()
