68707bc7e360032f76672a9b52a8e3dc1b1b0480
Kyreria <dennishaandrikman@live.nl>
2024-08-26 21:57:34 +0200

    Variable name changes for better clarity and updating the comments.

--- workflow/Scripts/Random_fasta_generator.py
+++ workflow/Scripts/Random_fasta_generator.py
@@ -1,5 +1,5 @@
 """
-This script generates a random FASTA file for use with the Snakemake Pipeline.
+This script generates random forward and reverse FASTA files for use with the Snakemake Pipeline.
 """
 
 
@@ -16,6 +16,7 @@ def reverse_complement(sequence):
 
 
 def barcode_selector():
+    # Select one of the barcodes with a chance of not having one, this is the "unknown barcode".
     barcodes = ["GAGAGTGT", "GAGTCACT", ""]
     return ''.join(random.choices(barcodes, weights=[10, 10, 1]))
 
@@ -30,29 +31,32 @@ def readname_generator(namelength):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(namelength))
 
 
-def random_fasta_generator(readcount, filename):
+def random_fasta_generator(read_count, filename):
     # Variables for the fasta generator
-    namelength = 12
-    sequencelength = 160
+    name_length = 12
+    sequence_length = 160
     forward_file = filename + "_R1" + ".fasta"
     reverse_file = filename + "_R2" + ".fasta"
 
     # This for loop generates the files
-    for i in range(readcount):
-        readname = ">" + readname_generator(namelength)
-        sequence = barcode_selector() + dna(sequencelength)
+    for i in range(read_count):
+        read_name = ">" + readname_generator(name_length)
+        sequence = barcode_selector() + dna(sequence_length)
         reverse_sequence = reverse_complement(sequence)
         # Write the forward read.
         with open(forward_file, "a") as fw:
-            fw.write(readname + "\n" + sequence + "\n")
+            fw.write(read_name + "\n" + sequence + "\n")
         # Write the reverse read
         with open(reverse_file, "a") as rw:
-            rw.write(readname + "\n" + reverse_sequence + "\n")
+            rw.write(read_name + "\n" + reverse_sequence + "\n")
 
 
 if __name__ == '__main__':
+    # Parse the arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--length", type=int, help="Length of the FASTA", required=True)
     parser.add_argument("-o", "--output", help="Output name for the fasta files", required=True)
     args = parser.parse_args()
+
+    # Run the generator
     random_fasta_generator(args.length, args.output)
