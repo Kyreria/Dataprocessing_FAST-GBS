65ee605f663bdef5db5444bae573af4dbce61ecd
Kyreria <dennishaandrikman@live.nl>
2024-08-28 00:46:12 +0200

    Changed the fasta generator to a fastq generator, this means it now also generates dummy quality scores for the reads.

--- workflow/Scripts/Random_fastq_generator.py
+++ workflow/Scripts/Random_fastq_generator.py
@@ -32,7 +32,7 @@ def read_name_generator(name_length):
                    for _ in range(name_length))
 
 
-def random_fasta_generator(read_count, file_name):
+def random_fastq_generator(read_count, file_name):
     # Variables for the fasta generator
     name_length = 12
     sequence_length = 160
@@ -41,17 +41,23 @@ def random_fasta_generator(read_count, file_name):
 
     # This for loop generates the files
     for i in range(read_count):
-        read_name = ">" + read_name_generator(name_length)
-        sequence = barcode_selector() + dna(sequence_length)
+        read_name = "@" + read_name_generator(name_length)
+
+        # These 2 sequences are for debugging
+        # sequence = barcode_selector() + dna(sequence_length)
+        sequence = dna(sequence_length)
+
         reverse_sequence = reverse_complement(sequence)
+        quality_score = ''.join('F' for _ in range(sequence_length))
+
         # Write the forward read.
         with open(forward_file, "a") as fw:
             read_name_forward = read_name + ".1"
-            fw.write(read_name_forward + "\n" + sequence + "\n")
+            fw.write(read_name_forward + "\n" + sequence + "\n" + "+" + "\n" + quality_score + "\n")
         # Write the reverse read
         with open(reverse_file, "a") as rw:
             read_name_reverse = read_name + ".2"
-            rw.write(read_name_reverse + "\n" + reverse_sequence + "\n")
+            rw.write(read_name_reverse + "\n" + reverse_sequence + "\n" + "+" + "\n" + quality_score + "\n")
 
 
 if __name__ == '__main__':
@@ -64,4 +70,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Run the generator
-    random_fasta_generator(args.length, args.output)
+    random_fastq_generator(args.length, args.output)
