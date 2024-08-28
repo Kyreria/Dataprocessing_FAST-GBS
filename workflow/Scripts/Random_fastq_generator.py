56a40c40178212d1331dcd37f176d9281e80a416
Kyreria <dennishaandrikman@live.nl>
2024-08-28 02:35:36 +0200

    Added comments to the whole script.

--- workflow/Scripts/Random_fastq_generator.py
+++ workflow/Scripts/Random_fastq_generator.py
@@ -1,5 +1,6 @@
 """
-This script generates random forward and reverse FASTA files for use with the Snakemake Pipeline.
+This script generates random forward and reverse FASTQ files for use with the Snakemake Pipeline.
+This is only for dummy data and testing purposes.
 """
 
 
@@ -10,29 +11,56 @@ import string
 
 
 def reverse_complement(sequence):
+    """
+    This function reverse complements the given DNA sequence using a dict.
+    :param sequence: A string, the given DNA sequence to reverse complement.
+    :return: Returns the reverse complemented DNA sequence
+    """
     # Reverse complements
     complement = {'A': 'T', 'C': 'G'}
     return ''.join(complement.get(base, base) for base in reversed(sequence))
 
 
 def barcode_selector():
+    """
+    This function uses a generator object together with random.choices to choose a random barcode
+    depending on the weights attached.
+    :return: Returns one of the selected barcodes as a string.
+    """
     # Select one of the barcodes with a chance of not having one, this is the "unknown barcode".
     barcodes = ["GAGAGTGT", "GAGTCACT", ""]
     return ''.join(random.choices(barcodes, weights=[10, 10, 1]))
 
 
 def dna(length):
+    """
+    This function uses a generator object and only the four letters of DNA 'CGTA'
+    to make a random DNA sequence for the length that was requested.
+    :param length: An integer, representing the length of the random DNA sequence
+    :return: Returns the random DNA sequence string.
+    """
     # Generates a random DNA strand based on the provided length
     return ''.join(random.choice('CGTA') for _ in range(length))
 
 
 def read_name_generator(name_length):
+    """
+    This function uses a generator object to randomly make string consisting of letters and numbers.
+    :param name_length: an int, how long the read name should be.
+    :return: returns the read name string.
+    """
     # Generates a random filler read_name generator to go behind the > in the fasta.
     return ''.join(random.choice(string.ascii_uppercase + string.digits)
                    for _ in range(name_length))
 
 
 def random_fastq_generator(read_count, file_name):
+    """
+    This is the generator for the fastq files.
+    :param read_count: An int, the amount of reads to generate
+    :param file_name: A string, the name of the fastq forward and reverse files to generate
+    :return: Doesn't return anything, produces 2 files instead, a forward and reverse fastq file.
+    """
     # Variables for the fasta generator
     name_length = 12
     sequence_length = 160
